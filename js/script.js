26846e9459c701729d571641af052e2ea35172b2
Ola-Reda <ola.reda.900@gmail.com>
2023-10-31 14:29:24 +0200

    Add changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const searchValue = document.querySelector('.search')
+const searchInput = document.querySelector('.search')
 const taskInput = document.querySelector('.task')
 const addBtn = document.querySelector('.add-btn');
 const saveBtn = document.querySelector('.save-btn');
@@ -37,13 +37,13 @@ function addTasksToPage(tasks) {
                 <td>${index+1}</td>
                 <td>${task.name}</td>
                 <td>
-                    <button class="btn edit-btn" onclick="editTask()">
+                    <button class="btn edit-btn" onclick="editTask(${index})">
                         <i class="fa-regular fa-pen-to-square"></i>
                         Edit
                     </button>
                 </td>
                 <td>
-                    <button class="btn complete-btn" id=${task.id}>
+                    <button class="btn complete-btn" id=${task.id}  >
                         <i class="fa-regular fa-square-check"></i>
                         Complete
                     </button>
@@ -59,40 +59,57 @@ function addTasksToPage(tasks) {
     table.innerHTML = data
     addToStorage(tasks)
 }
+
 //add to localstorage
 function addToStorage(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-
 //edit function 
-function editTask() {
+function editTask(index) {
+    taskInput.focus()
+    let saveTask = document.querySelector('.hidden-task')
+    saveTask.value = index
+    let task = JSON.parse(localStorage.getItem('tasks'))[index].name
+    taskInput.value = task
     addBtn.style.display = "none"
     saveBtn.style.display = "block"
 }
 
-//save
+//save task after edit
 saveBtn.addEventListener('click', saveTask)
 function saveTask() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    saveTask = document.querySelector('.hidden-task').value
+    for(let keys in tasks[saveTask]){
+        if(keys == 'name'){
+            tasks[saveTask].name = taskInput.value;
+        }
+    }
+    addToStorage(tasks)
     addBtn.style.display = "block"
     saveBtn.style.display = "none"
+    addTasksToPage(tasks)
+    taskInput.value = "";
 }
 
+
+
 //complete function 
-const completeBtns = document.querySelectorAll('.complete-btn')
-completeBtns.forEach(btn => {
-    return btn.addEventListener('click', function(e){
-        let completeTask = e.target.parentElement.previousElementSibling.previousElementSibling
-        completeTask.classList.toggle('complete')
-        taskId = e.target.getAttribute('id')
-        for(let i=0; i< tasksArray.length; i++){
-        if(tasksArray[i].id === taskId) {
-            tasksArray[i].complete === false ? tasksArray[i].complete = true : tasksArray[i].complete = false
-            }
-        }
-    addToStorage(tasksArray)
-    })
+table.addEventListener('click', function(e) {
+    if(e.target.classList.contains('complete-btn')) {
+        e.target.parentElement.previousElementSibling.previousElementSibling.classList.toggle('complete')
+    }
+    toggleStatus (e.target.getAttribute("id"))
 })
+function toggleStatus(id) {
+    for (let i=0 ; i< tasksArray.length; i++){
+        if (tasksArray[i].id == id) {
+            tasksArray[i].complete == false ? tasksArray[i].complete = true : tasksArray[i].complete = false
+        }
+    }
+    addToStorage(tasksArray)
+}
 
 
 //delete function 
@@ -109,4 +126,12 @@ function deleteAllTasks() {
 localStorage.clear()
 tasksArray = []
 addTasksToPage(tasksArray)
+}
+
+//search
+searchInput.addEventListener("input", search)
+function search() {
+    let value = searchInput.value
+    let tasks = tasksArray.filter(task => task.name.includes(value))
+    addTasksToPage(tasks)
 }
